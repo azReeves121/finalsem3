0c6f0aa91211fc954d87fe957c32f6ff621ee264
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2024-12-17 17:28:35 -0330

    login

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(
 
 // MongoDB Connection
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
